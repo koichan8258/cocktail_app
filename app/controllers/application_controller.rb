aaaac8c0720dc67998beb70cddffc88544966fa0
koichan8258 <iohikarinohito2@gmail.com>
2024-03-30 15:08:36 +0900

    訂正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 # app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
   skip_before_action :authenticate_user!, only: [:new, :create]
 
   private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'koide' && password == '8258'
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
