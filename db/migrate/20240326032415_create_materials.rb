36c33a9748550ec06bd47c8f290b4fc2be8c4340
koichan8258 <iohikarinohito2@gmail.com>
2024-03-30 18:33:05 +0900

    編集

--- db/migrate/20240326032415_create_materials.rb
+++ db/migrate/20240326032415_create_materials.rb
@@ -1,6 +1,7 @@
 class CreateMaterials < ActiveRecord::Migration[7.0]
   def change
     create_table :materials do |t|
+      t.string :material_name    
       t.string :quantity
       t.references :recipe, null: false, foreign_key: true
 
