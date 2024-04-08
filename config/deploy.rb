58facb4d694a8a785c9e64c556b42b0fabe97292
koichan8258 <iohikarinohito2@gmail.com>
2024-04-08 14:51:53 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.18.1"
 
 # Capistranoのログの表示に利用する
-set :application, 'COCKTAIL APP'
+set :application, 'cocktail_app'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:koichan8258/cocktail_app.git'
